dbddb802795d4a0fb9a8775ffb66cda425b2c5d1
Kirk Silva <kirk.ki@hotmail.com>
2024-05-07 17:57:46 -0300

    Ajuste Sidenav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    SidebarComponent,
+    SidenavComponent,
     DashboardComponent
   ],
   imports: [
