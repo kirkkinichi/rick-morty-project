6cb1d0affecfc3e6ddaa3d34f03ce44bef3dd915
Kirk Silva <kirk.ki@hotmail.com>
2024-05-09 11:13:08 -0300

    Adicionando rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes
+import { CharactersComponent } from './components/characters/characters.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { EpisodesComponent } from './components/episodes/episodes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'personagens', component: CharactersComponent },
+  { path: 'localizacoes', component: LocationsComponent },
+  { path: 'episodios', component: EpisodesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
