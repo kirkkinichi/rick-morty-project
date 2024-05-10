9db614c34bda28bc6f3c824999d3a23bbdd87890
Kirk Silva <kirk.ki@hotmail.com>
2024-05-10 13:27:48 -0300

    Adicionando scroll infinito, api episodes e remoção do service.

--- src/app/components/episodes/episodes.component.ts
+++ src/app/components/episodes/episodes.component.ts
@@ -1,26 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { RickmortyApiService } from '../../services/rickmorty-api.service';
+import { Component, HostListener } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-episodes',
   templateUrl: './episodes.component.html',
   styleUrl: './episodes.component.scss'
 })
-export class EpisodesComponent implements OnInit{
+export class EpisodesComponent {
 
-  public getAllEpisodes: any;
+  episodes: any[] = []; //Armazena personagens da API
+  episodeUrl: string | null = 'https://rickandmortyapi.com/api/episode'; // Armazena a próxima página a ser carregada
+  loading = false; //Indica o estado da solicitação HTTP
 
-  constructor(
-    private rickmortyApiService: RickmortyApiService
-  ) { }
+  //Solicitação HTTP
+  constructor(private http: HttpClient) {
+    this.checkLoadEpisodes();
+  }
 
-  ngOnInit(): void {
+  //Carrega os personagens da API e veriffica se há uma solicitação HTTP em andamento
+  checkLoadEpisodes() {    
+    //Verifica se há uma solicitação em andamento / página para carregar
+    if (this.loading || !this.episodeUrl) {
+      return;
+    }
 
-    this.rickmortyApiService.listAllEpisodes.subscribe(    
-        
-      inf => {
-        this.getAllEpisodes = inf.results;
-      }
-    );
+    // Inicia solicitação
+    this.loading = true;
+
+    // Solicitação GET para obter os personagens e atualizar com a URL da próxima página
+    this.http.get(this.episodeUrl).subscribe((data: any) => {
+
+      this.episodes = this.episodes.concat(data.results);
+      this.episodeUrl = data.info.next;
+      this.loading = false; // Atribuído valor false para indicar que a solicitação foi concluída
+    });
+  }
+
+  //Método chamado ao ocorrer o rolamento / scroll na janela
+  @HostListener('window:scroll', ['$event'])
+  scrollAction() {
+
+    //Variáveis para calcular o scroll da página
+    const height = window.innerHeight;
+    const scroll = window.scrollY;
+    const totalHeight = document.body.offsetHeight;
+
+    //Calculo para verificar se o usuário está próximo ao fim da página, a uma distância de 200 pixels para garantir que a detecção aconteça antes do final da página
+    const nearEnd = height + scroll >= totalHeight - 200;
+
+    //Chama o método checkLoadEpisodes() para carregar mais personagens
+    if (nearEnd) {
+      this.checkLoadEpisodes();
+    }
   }
 }
