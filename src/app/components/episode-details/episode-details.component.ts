1eb2bd90bec253e4fcd81317c659b610b3baa74a
Kirk Silva <kirk.ki@hotmail.com>
2024-05-10 17:52:13 -0300

    Adicionando detalhes para cada episódio.

--- src/app/components/episode-details/episode-details.component.ts
+++ src/app/components/episode-details/episode-details.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
 
 @Component({
-  selector: 'app-episode-details',
-  templateUrl: './episode-details.component.html',
-  styleUrl: './episode-details.component.scss'
+	selector: 'app-episode-details',
+	templateUrl: './episode-details.component.html',
+	styleUrl: './episode-details.component.scss'
 })
-export class EpisodeDetailsComponent {
+export class EpisodeDetailsComponent implements OnInit {
 
+	episodeId: number | undefined;
+	episode: any;
+	characters: any[] = [];
+
+	constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  	ngOnInit(): void {
+    	this.episodeId = this.route.snapshot.params['id'];
+		this.getEpisodeDetails();
+  	}
+
+	getEpisodeDetails() {
+
+		if (this.episodeId) {
+			this.http.get(`https://rickandmortyapi.com/api/episode/${this.episodeId}`).subscribe((data: any) => {
+				
+				this.episode = data;
+
+				this.getCharacterDetails(this.episode.characters);
+			});
+		}
+	}
+
+	//Função para receber os valores dos personagens
+	getCharacterDetails(characterUrl: string[]) {
+		// Mapeia cada URL do episódio para uma solicitação HTTP e as combina em uma única solicitação
+		const requests = characterUrl.map(url => this.http.get(url));
+
+		forkJoin(requests).subscribe((characters: any[]) => {
+
+			this.characters = characters;
+		});
+	}
 }
