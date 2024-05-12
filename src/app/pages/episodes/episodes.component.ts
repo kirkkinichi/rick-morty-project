ae95c884f22f98b32e4d0eec1a0d3edf4f4a37a3
Kirk Silva <kirk.ki@hotmail.com>
2024-05-11 21:48:57 -0300

    Adicionando pesquisa de episódios.

--- src/app/pages/episodes/episodes.component.ts
+++ src/app/pages/episodes/episodes.component.ts
@@ -1,5 +1,11 @@
 import { Component, HostListener } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay } from '../../utils';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+export const DEFAULT_EPISODES_URL = 'https://rickandmortyapi.com/api/episode';
 
 @Component({
   selector: 'app-episodes',
@@ -8,49 +14,77 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EpisodesComponent {
 
-  episodes: any[] = []; //Armazena personagens da API
-  episodeUrl: string | null = 'https://rickandmortyapi.com/api/episode'; // Armazena a próxima página a ser carregada
-  loading = false; //Indica o estado da solicitação HTTP
+  episodes: any[] = []; // Armazena personagens da API
+	episodeUrl: string = DEFAULT_EPISODES_URL; // Armazena a próxima página a ser carregada
+	loading = false; // Indica o estado da solicitação HTTP
+	error: string = '';
 
-  //Solicitação HTTP
-  constructor(private http: HttpClient) {
-    this.checkLoadEpisodes();
-  }
+	constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
+		this.route.queryParams.subscribe(params => {
+			this.episodes = [];
+			const foundParamInput = params['inputParam']
+			if (foundParamInput && this.episodeUrl) {				
+				const newUrl = new URL(this.episodeUrl);
+				newUrl.searchParams.set('page', '1')
+				newUrl.searchParams.set('name', foundParamInput)
+				let manipulatedUrl = newUrl.toString().replace('?', "/?")
+				manipulatedUrl = newUrl.toString().replace('//?', "/?")
+				this.episodeUrl = manipulatedUrl
+			} else {
+				this.episodeUrl = DEFAULT_EPISODES_URL;
+			}
+			this.checkLoadEpisodes();
+		});
+	}
 
-  //Carrega os personagens da API e veriffica se há uma solicitação HTTP em andamento
-  checkLoadEpisodes() {    
-    //Verifica se há uma solicitação em andamento / página para carregar
-    if (this.loading || !this.episodeUrl) {
-      return;
-    }
 
-    // Inicia solicitação
-    this.loading = true;
+	// Carrega os personagens da API e verifica se há uma solicitação HTTP em andamento
+	checkLoadEpisodes() {
+		this.error = '';
+		// Verifica se há uma solicitação em andamento / página para carregar
+		if (this.loading || !this.episodeUrl) {
+			return;
+		}
+		// Inicia solicitação
+		this.loading = true;
 
-    // Solicitação GET para obter os personagens e atualizar com a URL da próxima página
-    this.http.get(this.episodeUrl).subscribe((data: any) => {
+		//Delay ao scrollar
+		delay(1500).then(() => {
+			// Solicitação GET para obter os personagens e atualizar com a URL da próxima página
+			this.http.get(this.episodeUrl).pipe(catchError((error) => {
+				this.handleError(error);
+				return throwError(error);
+			  })).subscribe((data: any) => {
+				this.episodes = this.episodes.concat(data.results);
+				this.episodeUrl = data.info.next;
+				this.loading = false; // Atribuído valor false para indicar que a solicitação foi concluída
+			});
+		});
+	}
 
-      this.episodes = this.episodes.concat(data.results);
-      this.episodeUrl = data.info.next;
-      this.loading = false; // Atribuído valor false para indicar que a solicitação foi concluída
-    });
-  }
+	handleError(error: HttpErrorResponse) {
+		this.loading = false;
+		if (error.status === 404) {
+			this.error = 'Nenhum personagem encontrado.'
+		} else {
+			this.error = 'Ops! Erro ao processar uma requisição.'
+		}
+	}
 
-  //Método chamado ao ocorrer o rolamento / scroll na janela
-  @HostListener('window:scroll', ['$event'])
-  scrollAction() {
+	// Método chamado ao ocorrer o scroll (rolamento) na janela
+	@HostListener('window:scroll', ['$event'])
+	scrollAction() {
+		// Variáveis para calcular o scroll da página
+		const height = window.innerHeight;
+		const scroll = window.scrollY;
+		const totalHeight = document.body.offsetHeight;
 
-    //Variáveis para calcular o scroll da página
-    const height = window.innerHeight;
-    const scroll = window.scrollY;
-    const totalHeight = document.body.offsetHeight;
+		// Cálculo para verificar se o usuário está próximo ao fim da página, a uma distância de 200 pixels para garantir que a detecção aconteça antes do final da página
+		const nearEnd = height + scroll >= totalHeight - 200;
 
-    //Calculo para verificar se o usuário está próximo ao fim da página, a uma distância de 200 pixels para garantir que a detecção aconteça antes do final da página
-    const nearEnd = height + scroll >= totalHeight - 200;
-
-    //Chama o método checkLoadEpisodes() para carregar mais personagens
-    if (nearEnd) {
-      this.checkLoadEpisodes();
-    }
-  }
+		// Chama o método checkLoadEpisodes() para carregar mais personagens
+		if (nearEnd && this.episodeUrl) {
+			this.checkLoadEpisodes();
+		}
+	}
 }
